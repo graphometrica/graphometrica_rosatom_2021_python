5eff0417089a46e9cc5638845f9a699929b8fcd9
SemyonSinchenko <semyonsinchenko@yahoo.com>
2021-10-24 07:41:06 +0300

    Add on_disconnected
    On branch master
    Changes to be committed:
    - modified:   solution/service/listeners/TSPListener.py

--- solution/service/listeners/TSPListener.py
+++ solution/service/listeners/TSPListener.py
@@ -1,4 +1,6 @@
+import os
 import stomp
+import time
 
 from ..serialization import Input, Result
 from ..solver import solve
@@ -20,3 +22,24 @@ class TSPListener(stomp.ConnectionListener):
         print('Sending result "%s"' % result.json())
 
         self.conn.send(body=result.json(), destination=self.queue)
+
+    def on_disconnected(self):
+        print("Disconnected... Try to reconnect.")
+        user = os.environ["MQ_USER"]
+        password = os.environ["MQ_PASSWORD"]
+        queue_in = os.environ["MQ_QUEUE_INPUT"]
+
+        attempts = 10
+        n = 0
+        while n < 10:
+            try:
+                self.conn.connect(user, password, wait=True, ack="auto")
+            except Exception as e:
+                n += 1
+                print(f"Attempt {n}: exception...")
+                time.sleep(5)
+                continue
+            else:
+                print("Reconnection was succesful!")
+                self.conn.subscribe(queue_in, id=1)
+                break
